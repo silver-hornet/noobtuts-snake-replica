6cdfcda7a03da99ecdbcbb94271412449beeb176
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-10-10 10:43:00 -0400

    Initial commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour
+{
+    // Current movement direction
+    // (by default it moves to the right
+    Vector2 dir = Vector2.right;
+
+    // Keep track of tail
+    List<Transform> tail = new List<Transform>();
+
+    // Did the snake eat something?
+    bool ate = false;
+
+    // Tail Prefab
+    [SerializeField] GameObject tailPrefab;
+
+    void Start()
+    {
+        // Move the snake every 300ms
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    void Update()
+    {
+        // Move in a new direction?
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = Vector2.down; // Could also be accomplished with dir = -Vector2.up
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = Vector2.left; // Could also be accomplished with dir = -Vector2.right
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+
+    void Move()
+    {
+        // Save current position (gap will be here)
+        Vector2 v = transform.position;
+
+        // Move head into new direction
+        transform.Translate(dir);
+        // transform.Translate means 'add this vector to my position'
+
+        // Ate something? Then insert new element into gap
+        if (ate)
+        {
+            // Load prefab into the world
+            GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+
+            // Keep track of it in our tail list
+            tail.Insert(0, g.transform);
+
+            // Reset the flag
+            ate = false;
+        }
+
+        // Do we have a tail?
+        if (tail.Count > 0)
+        {
+            // Move last tail element to where the head was
+            tail.Last().position = v;
+
+            // Add to front of list, remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Food?
+        if (collision.name.StartsWith("FoodPrefab"))
+        // We use collision.name.StartsWith because the food is called 'FoodPrefab(Clone)' after instantiating it.
+        // The more elegant way to find out if coll is food or not would be by using a Tag, but for the sake of simplicity we will use string comparison here.s
+        {
+            // Get longer in next move call
+            ate = true;
+
+            // Remove the food
+            Destroy(collision.gameObject);
+        }
+
+        // Collided with tail or border
+        else
+        {
+            // TODO 'You Lose' screen
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    // Food Prefab
+    [SerializeField] GameObject foodPrefab;
+
+    // Borders
+    [SerializeField] Transform borderTop;
+    [SerializeField] Transform borderBottom;
+    [SerializeField] Transform borderLeft;
+    [SerializeField] Transform borderRight;
+
+    void Start()
+    {
+        // Spawn food every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    void Spawn()
+    {
+        // x position between left and right border
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        // (int) rounds the number to make sure that the food is always spawned at a position like (1, 2) but never at something like (1.234, 2.74565).
+
+        // y position between top and bottom border
+        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+        // (int) rounds the number to make sure that the food is always spawned at a position like (1, 2) but never at something like (1.234, 2.74565).
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity); // default rotation
+    }
+}
